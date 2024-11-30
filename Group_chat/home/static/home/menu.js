337e4d30e98486deeb3683a40325574391f9464d
wthobika <wthobika@buffalo.edu>
2024-11-30 13:19:35 -0500

    incomplete ws homepage

--- Group_chat/home/static/home/menu.js
+++ Group_chat/home/static/home/menu.js
@@ -6,3 +6,21 @@ function dropdown(){
         document.getElementById("dropdown").className = "dropdown-hidden";
     }
 }
+
+function onload(){
+    initWS();
+}
+
+function initWS(){
+    let url = 'ws://' + window.location.host + '/scheme'
+    socket = new WebSocket(url)
+
+    socket.onmessage = function (ws_message) {
+        likes_data = JSON.parse(ws_message.data)
+        // console.log("likes data ->")
+        // console.log(likes_data)
+        message = likes_data["message"]
+        // console.log(JSON.parse(ws_message.data))
+        updateLikes(ws_message.data, tripID)
+    }
+}
